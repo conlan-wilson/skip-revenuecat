a571781c929c0241925b6c5245a7a5b4c87885d7
Conlan Wilson <conlan_wilson@hotmail.com>
2025-10-06 15:23:29 -0700

    Updated skip version

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ let package = Package(
             ])
     ],
     dependencies: [
-        .package(url: "https://source.skip.tools/skip.git", exact: "1.6.5"),
+        .package(url: "https://source.skip.tools/skip.git", from: "1.6.5"),
         .package(url: "https://source.skip.tools/skip-foundation.git", exact: "1.3.8"),
         .package(url: "https://github.com/RevenueCat/purchases-hybrid-common.git", exact: "15.0.0"),
     ],
